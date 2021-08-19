73900d0d086bcc4554bcf54894c7b8d133a95348
= <janssenbailio@gmail.com>
2021-08-20 02:28:57 +0800

    separated mongo uri to a env file

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 app.use(express());
 app.use(express.json());
+require("dotenv").config();
+
+if (!process.env.MONGO_URI) {
+  console.error(
+    `No "MONGO_URI" variable set. Please set an env variable with the db uri as the value`
+  );
+  process.exit(1);
+}
 
 require("./startup/db")();
 require("./startup/prod")(app);
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
-
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://jajabailio:abcdefg123123@coffee.v9vhy.mongodb.net/companly?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
 
 module.exports = function () {
-    mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-        .then(() => console.log('MongoDB connected...'))
-        .catch((err) => { console.error('Mongoose connect error: ', err) });
-}
+  mongoose
+    .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected..."))
+    .catch((err) => {
+      console.error("Mongoose connect error: ", err);
+    });
+};
